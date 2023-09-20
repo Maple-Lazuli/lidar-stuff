6ab24e0cd6a584bf5f129f0ba77ec687c712659b
Maple-Lazuli <the.nostra.tymus@gmail.com>
2023-09-19 21:18:08 -0400

    added a render to the point cloud

--- src/environment.cpp
+++ src/environment.cpp
@@ -46,9 +46,12 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // TODO:: Create lidar sensor 
-    Lidar* lidar = new Lidar(cars, 0)
+    Lidar* lidar = new Lidar(cars, 0);
     // TODO:: Create point processor
-  
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pointCloud = lidar->scan();
+    renderRays(viewer, lidar->position, pointCloud);
+
 }
 
 
