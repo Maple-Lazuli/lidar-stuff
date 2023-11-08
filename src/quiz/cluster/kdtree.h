298e60da676b34bbef60d6d62505469750f4ec78
Maple-Lazuli <the.nostra.tymus@gmail.com>
2023-11-08 12:25:26 -0500

    implemented the kd tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -38,8 +38,28 @@ struct KdTree
 
 	void insert(std::vector<float> point, int id)
 	{
+
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
+
+		insert_recursive(&root, 0, point, id);
+
+	}
+
+	void insert_recursive(Node** node, int depth, std::vector<float> point, int id) {
+
+		// At a null, insert the node.
+		if (*node==NULL)
+			*node= new Node(point, id);
+		else {
+			
+			int current_dimension = depth % 2; //get the current dimension to check. 
+
+			if(point[current_dimension] < ((*node) -> point[current_dimension]))
+				insert_recursive( &((*node) -> left), depth + 1, point, id);
+			else
+				insert_recursive( &((*node)->right), depth + 1, point, id);
+		}
 
 	}
 
